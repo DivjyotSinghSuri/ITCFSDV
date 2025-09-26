07e0590228189fc617ab4eb6048e7560eeb67b79
DivjyotSinghSuri <divjyotsuri2005@gmail.com>
2025-09-26 12:55:31 +0530

    26 sept

--- JS/index.js
+++ JS/index.js
@@ -1,143 +1,195 @@
-// console.log("HELLO")
-// var a = 20;
-// console.log("a=" + a);
+// // console.log("HELLO")
+// // var a = 20;
+// // console.log("a=" + a);
 
-// console.log(a);
-// var a=20;
+// // console.log(a);
+// // var a=20;
 
-// var a=50;
-// if(a>10){
-//   var a=100;
-//   console.log("a="+a);
-// }else{
-//   console.log("hhhhiiiii")
+// // var a=50;
+// // if(a>10){
+// //   var a=100;
+// //   console.log("a="+a);
+// // }else{
+// //   console.log("hhhhiiiii")
+// // }
+// // console.log("hiii"+a);
+
+// // const a=23;
+// // a=50;
+
+// //NORMAL FUNCTION
+
+// // function fun(msg){
+
+// //   return "Hey..."+msg;
+// //   console.log("Helllo")
+// // }
+
+// // const data = fun("greeting..");
+// // console.log(data);
+
+// // FUNCTION AS AN EXPRESSION
+
+// // const data=function(a,b){
+// //   return a*b;
+// // }
+
+// // console.log(data(12,5));
+
+// // ARROW FUNCTION
+
+// // const data=(msg)=>{
+// //   console.log("Hey.. using arrow function "+msg)
+// // }
+// // data("in react development");
+
+// // const data=msg=>msg;
+// // const output = data("Greeting of the day");
+// // console.log(output);
+
+// //IIFE
+
+// // (function(){
+// //   console.log("Welcome to MERN session")
+// // })();
+
+// // setTimeout(()=> {
+// //   console.log("hiiii")
+// // },1000);
+
+// // function greet(msg="How have you been?"){
+// //   console.log(msg+" Good");
+// // }
+// // greet();
+// // greet("How are you?");
+
+// // function selectlanguage(lang="Python"){
+// //   let result;
+// //   if(lang=="java"){
+// //     function javaCompiler(){
+// //       return "Using Java Compiler"
+// //     }
+
+// //     result = javaCompiler();
+// //   }
+// //   else if(lang=="c"){
+// //     function Ccompiler(){
+// //       return "Using C compiler"
+// //     }
+
+// //     result=Ccompiler();
+// //   } 
+// //   else if(lang=="Python"){
+// //     function PythonCompiler(){
+// //       return "Using Python compiler"
+// //     }
+
+// //     result=PythonCompiler();
+// //   }
+// //   else{
+// //     result="No compiler found";
+// //   }
+// // return result;
+// // }
+
+// // console.log(selectlanguage());
+
+
+// // const div=document.getElementsByTagName("div");
+
+// // div[0].innerText="Hello";
+// // div[0].style.color='red';
+// // div[0]. style.backgroundColor='green';
+// // console.log(div);
+
+// const container = document.getElementsByClassName("container");
+
+// // container[0].innerHTML="<h2 style.color:'red'> Hello </h2>"
+// // console.dir(container);
+
+// const h2 = document.createElement('h2');
+// h2.innerText = "ABES ENGINEERING COLLEGE";
+// h2.style.color = "maroon";
+// h2.style.backgroundColor = "yellow";
+// console.log(h2);
+// container[0].appendChild(h2);
+
+
+// const button = document.getElementById('btn');
+// console.log(button);
+
+// document.getElementById('disp').innerHTML = "<h4>loading image...</h4>";
+
+// function msg() {
+//   const h3 = document.createElement('h3');
+//   h3.innerText = "Welcome to ABES ENGINEERING COLLEGE";
+//   h3.style.color = "yellow";
+//   h3.style.backgroundColor = "maroon";
+//   container[0].appendChild(h3);
+
+//   const img = document.createElement('img');
+//   img.src = 'https://static.vecteezy.com/system/resources/thumbnails/036/324/708/small/ai-generated-picture-of-a-tiger-walking-in-the-forest-photo.jpg';
+//   img.setAttribute("height", "200px");
+//   img.setAttribute("width", "450px");
+//   console.log(img);
+//   container[0].appendChild(img);
+
+//   document.getElementById('disp').innerHTML = "";
+
+//   // console.log("Hi, i am working on MERN stack.");
+//   // alert("Hi, i am using DOM.")
+
 // }
-// console.log("hiii"+a);
+// button.addEventListener('click', msg);
 
-// const a=23;
-// a=50;
 
-//NORMAL FUNCTION
-
-// function fun(msg){
-
-//   return "Hey..."+msg;
-//   console.log("Helllo")
-// }
-
-// const data = fun("greeting..");
-// console.log(data);
-
-// FUNCTION AS AN EXPRESSION
-
-// const data=function(a,b){
-//   return a*b;
-// }
-
-// console.log(data(12,5));
-
-// ARROW FUNCTION
-
-// const data=(msg)=>{
-//   console.log("Hey.. using arrow function "+msg)
-// }
-// data("in react development");
-
-// const data=msg=>msg;
-// const output = data("Greeting of the day");
-// console.log(output);
-
-//IIFE
-
-// (function(){
-//   console.log("Welcome to MERN session")
-// })();
-
-// setTimeout(()=> {
-//   console.log("hiiii")
-// },1000);
-
-// function greet(msg="How have you been?"){
-//   console.log(msg+" Good");
-// }
-// greet();
-// greet("How are you?");
-
-// function selectlanguage(lang="Python"){
-//   let result;
-//   if(lang=="java"){
-//     function javaCompiler(){
-//       return "Using Java Compiler"
+//const button = document.getElementById("bin");
+// function longData(){
+//     console.log("Start");
+//     for(i=0;i<100000000;i++){
+//         console.log(i)
 //     }
+//     console.log("End");
+// }
+// button.addEventListener("click", longData);
 
-//     result = javaCompiler();
-//   }
-//   else if(lang=="c"){
-//     function Ccompiler(){
-//       return "Using C compiler"
-//     }
-
-//     result=Ccompiler();
-//   } 
-//   else if(lang=="Python"){
-//     function PythonCompiler(){
-//       return "Using Python compiler"
-//     }
-
-//     result=PythonCompiler();
-//   }
-//   else{
-//     result="No compiler found";
-//   }
-// return result;
+// function greetmsg(){
+//     return "Welcome to ABES College"
 // }
 
-// console.log(selectlanguage());
+// function msgHandler(msg, clbk){
+//     // console.log(msg);
+//     const data = clbk(msg);
+//     return data;
+// }
+// const completemsg = msgHandler("Hii", greetmsg);
+// console.log(completemsg)
+
+// const student = ["Rahul", "Aman", "Vansh"]
+// student.forEach((name)=>{
+//   console.log(msgHandler(name,greetmsg))
+// })
+
+//PROMISE
+const mypromise=new Promise((resolve,reject)=>{
+const password='987ghzn21';
+if (password.length>8){
+  resolve("Password length is ok.");}
+else { 
+    reject("Password length is not as per our policy");
+  }
+});
 
 
-// const div=document.getElementsByTagName("div");
+mypromise.then((result)=>{console.log(result)})
+.catch((err)=>{console.log(err)})
+.finally(()=>{
+  console.log("All the resources have closed succesfully");
+})
 
-// div[0].innerText="Hello";
-// div[0].style.color='red';
-// div[0]. style.backgroundColor='green';
-// console.log(div);
-
-const container = document.getElementsByClassName("container");
-
-// container[0].innerHTML="<h2 style.color:'red'> Hello </h2>"
-// console.dir(container);
-
-const h2 = document.createElement('h2');
-h2.innerText = "ABES ENGINEERING COLLEGE";
-h2.style.color = "maroon";
-h2.style.backgroundColor = "yellow";
-console.log(h2);
-container[0].appendChild(h2);
-
-
-const button = document.getElementById('btn');
-console.log(button);
-
-document.getElementById('disp').innerHTML = "<h4>loading image...</h4>";
-
-function msg() {
-  const h3 = document.createElement('h3');
-  h3.innerText = "Welcome to ABES ENGINEERING COLLEGE";
-  h3.style.color = "yellow";
-  h3.style.backgroundColor = "maroon";
-  container[0].appendChild(h3);
-
-  const img = document.createElement('img');
-  img.src = 'https://static.vecteezy.com/system/resources/thumbnails/036/324/708/small/ai-generated-picture-of-a-tiger-walking-in-the-forest-photo.jpg';
-  img.setAttribute("height", "200px");
-  img.setAttribute("width", "450px");
-  console.log(img);
-  container[0].appendChild(img);
-
-  document.getElementById('disp').innerHTML = "";
-
-  // console.log("Hi, i am working on MERN stack.");
-  // alert("Hi, i am using DOM.")
-
+function handleData(){
+  // console.log(mypromise);
+  const data = await mypromise;
+  console.log(data);
 }
-button.addEventListener('click', msg);
+handleData();

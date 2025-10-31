535dc0cef927458bbab44363b0a60aa0d756fb1a
DivjyotSinghSuri <divjyotsuri2005@gmail.com>
2025-10-31 11:49:01 +0530

    31 oct

--- Reactwork/reactviteapp/src/App.jsx
+++ Reactwork/reactviteapp/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Profile from './component/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <h2>WELCOME to react and vite App.</h2>
-    </>
+    <div>
+      <h2>WElcome to react and vite App</h2>
+      <h3>
+        <Profile/>
+      </h3>
+    </div>
   )
 }
 

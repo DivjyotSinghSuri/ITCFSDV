5f73f00d920c49a83ab772e2cb6a24dab1ecc1b3
DivjyotSinghSuri <divjyotsuri2005@gmail.com>
2025-11-28 12:21:14 +0530

    28th nov

--- Reactwork/shoppingCart/src/App.jsx
+++ Reactwork/shoppingCart/src/App.jsx
@@ -1,26 +1,30 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './component/Login';
-import Registration from './component/Registration';
-import Dashboard from './component/Dashboard';
-import FinalLayout from './component/FinalLayout';
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './component/Login'
+import Registration from './component/Registration'
+import Dashboard from './component/Dashboard'
+import FinalLayout from './component/FinalLayout'
 
 function App() {
+ const[data,setData]=useState();
+
   return (
-    <> {/* React Fragment */}
+    <>
+      {/* <h2>Welcome to Shopping Cart</h2> */}
       <BrowserRouter>
-        <Routes>
-          {/* Define the route and its component */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Registration/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/finallayout" element={<FinalLayout/>} />
-        </Routes>
+      <Routes>
+        <Route path='/login' element={<Login logData={data}/>}></Route>
+        <Route path='/register' element={<Registration regData={setData}/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/' element={<FinalLayout/>} />
+      </Routes>
       </BrowserRouter>
+      {JSON.stringify(data)}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
